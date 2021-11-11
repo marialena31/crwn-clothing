690478215b7b3c3379c9c3e91e88410396805039
Maria'lena Pietri <marialena.pietri@maps-system.com>
2021-11-11 22:00:43 +0100

    Initial commit

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link} from 'react-router-dom';
+
+import { auth } from '../../firebase/firebase.utils';
+
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+
+import './header.styles.scss';
+
+const Header = ({ currentUser }) => (
+    <div className='header'>
+        <Link to="/">
+            <Logo className='logo' />
+        </Link>
+        <div className='options'>
+            <Link className='option' to='/shop'>
+                SHOP
+            </Link>
+            <Link className='option' to='/shop'>
+                CONTACT
+            </Link>
+            {
+                currentUser ?
+                <a className='option' onClick={() => auth.signOut()}>
+                    SIGN OUT
+                </a>
+                :
+                <Link className='option' to='/signin'>
+                    SIGN IN
+                </Link>
+            }
+        </div>
+    </div>
+
+);
+export default Header;
